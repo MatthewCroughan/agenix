8d2fc2686d0cade5b1bdd4fc963e539e09cca7c8
matthewcroughan <matt@croughan.sh>
2021-09-10 00:54:36 +0100

    wip

--- modules/age.nix
+++ modules/age.nix
@@ -111,10 +111,7 @@ in
 
     # Secrets with root owner and group can be installed before users
     # exist. This allows user password files to be encrypted.
-    system.activationScripts.agenixRoot = { 
-      supportsDryActivation = false;
-      text = stringAfter [ "specialfs" ] installRootOwnedSecrets;
-    }; 
+    system.activationScripts.agenixRoot = stringAfter [ "specialfs" ] installRootOwnedSecrets + stringAfter [ "specialfs" ] installRootOwnedSecrets;
     system.activationScripts.users.deps = [ "agenixRoot" ];
 
     # Other secrets need to wait for users and groups to exist.
